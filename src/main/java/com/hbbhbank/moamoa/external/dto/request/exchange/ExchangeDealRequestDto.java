f6de6de4cdbe1b84ce71ee5da66b71e8fd4ed840
yezanee <rbfrk02@naver.com>
2025-05-28 13:52:57 +0900

    remove: 불필요한 포인트 환전 관련 Dto 삭제

--- src/main/java/com/hbbhbank/moamoa/external/dto/request/exchange/ExchangeDealRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.hbbhbank.moamoa.external.dto.request.exchange;
-
-public record ExchangeDealRequestDto(
-  String quoteId,            // 환율 견적 ID
-  String amount,             // 환전할 금액 (문자열로 전달할 수도 있음, 필요시 BigDecimal로 변경 가능)
-  String idempotencyKey      // 멱등성 키 (중복 방지용, UUID 권장)
-) {}
-
--- src/main/java/com/hbbhbank/moamoa/external/dto/request/exchange/ExchangeQuoteRequestDto.java
+++
@@ -1,7 +0,0 @@
-package com.hbbhbank.moamoa.external.dto.request.exchange;
-
-public record ExchangeQuoteRequestDto(
-  String fromCurrency,
-  String toCurrency,
-  String amount // API 요구사항에 따라 BigDecimal도 가능
-) {}
